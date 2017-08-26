0fb61c645c63565fd9e9ee7c2d98319485f3a85f
PrinceFrog2k <long2k1708@gmail.com>
2017-08-26 13:19:51 +0700

    Bai 3 de HSG HN 2008 (Debugging)

--- 2008/Bai3.pas
+++ 2008/Bai3.pas
@@ -1,7 +1,50 @@
 uses crt;
-type ArrType = array[1..100] of longint;
+type ArrType = array[-100..100, -100..100] of longint;
 var Arr: ArrType;
+	imax, jmax, i, j: longint;
+	fIn, fOut: text;
+	
+procedure initData;
+	var i, j: longint;
+	begin
+		fillchar(Arr, sizeof(Arr), 0);
+		assign(fIn, 'D:\PascalAssetFile\qm.in');
+		assign(fOut, 'D:\PascalAssetFile\qm.ou');
+		reset(fIn); rewrite(fOut);
+		read(fIn, jmax, imax);
+		for j := 1 to jmax do
+			for i := 1 to imax do read(fIn, Arr[i,j]);
+		close(fIn);
+	end;
+	
+function getMax(a, b, c, d: longint): longint;
+	begin
+		getMax := a;
+		if (b > getMax) then getMax := b;
+		if (c > getMax) then getMax := c;
+		if (d > getMax) then getMax := d;
+	end;
+	
+procedure Calculate;
+	var i, j: longint;
+	begin
+		for i := 1 to imax do
+			for j := 1 to jmax do
+			begin
+				Arr[i,j] := getMax(Arr[i-2, j-1], 
+								Arr[i-1, j-2], 
+								Arr[i+1, j-2], 
+								Arr[i+2, j-1]) + Arr[i,j];
+			end;
+	end;
 
 begin
-
+	initData;
+	Calculate;
+	for j := 1 to jmax do
+	begin
+		for i := 1 to imax do write(Arr[i,j]:3);
+		writeln
+	end;
+	readln
 end.
